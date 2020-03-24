7e7dad492c6c5989dd440f188748835d7f65014c
mountainash <mountainash@rmwpublishing.net>
2020-03-24 02:31:20 +0100

    Disabled "View" menu
    - until hooked back up again
    
    Releated: https://github.com/kreativgebiet/taskana/issues/6

--- menu/index.js
+++ menu/index.js
@@ -1,13 +1,13 @@
-
 const { Menu } = require('electron');
 
 const main = require('./main');
 const tasks = require('./tasks');
-const view = require('./view');
+//const view = require('./view');
 const edit = require('./edit');
 const window = require('./window');
 const help = require('./help');
 
-const template = [main, tasks, view, edit, window, help];
+//const template = [main, tasks, view, edit, window, help];
+const template = [main, tasks, edit, window, help];
 
 module.exports = Menu.buildFromTemplate(template);
